152a297255eb0ff472c78dbd9ec9b03723f290fb
unknown <Devang@coolstorybro.(none)>
2012-10-27 10:26:31 -0700

    Adding Helen's code to play audio

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusC = new System.Windows.Forms.Label();
+            this.statusD = new System.Windows.Forms.Label();
+            this.statusE = new System.Windows.Forms.Label();
+            this.statusG = new System.Windows.Forms.Label();
+            this.statusF = new System.Windows.Forms.Label();
+            this.howToText = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
+            this.labelE = new System.Windows.Forms.Label();
+            this.labelF = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
+            this.statusA = new System.Windows.Forms.Label();
+            this.statusB = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.statusC6 = new System.Windows.Forms.Label();
+            this.labelC6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // statusC
+            // 
+            this.statusC.AutoSize = true;
+            this.statusC.Location = new System.Drawing.Point(12, 75);
+            this.statusC.Name = "statusC";
+            this.statusC.Size = new System.Drawing.Size(27, 13);
+            this.statusC.TabIndex = 0;
+            this.statusC.Text = "OFF";
+            // 
+            // statusD
+            // 
+            this.statusD.AutoSize = true;
+            this.statusD.Location = new System.Drawing.Point(45, 75);
+            this.statusD.Name = "statusD";
+            this.statusD.Size = new System.Drawing.Size(27, 13);
+            this.statusD.TabIndex = 1;
+            this.statusD.Text = "OFF";
+            // 
+            // statusE
+            // 
+            this.statusE.AutoSize = true;
+            this.statusE.Location = new System.Drawing.Point(78, 75);
+            this.statusE.Name = "statusE";
+            this.statusE.Size = new System.Drawing.Size(27, 13);
+            this.statusE.TabIndex = 2;
+            this.statusE.Text = "OFF";
+            // 
+            // statusG
+            // 
+            this.statusG.AutoSize = true;
+            this.statusG.Location = new System.Drawing.Point(144, 75);
+            this.statusG.Name = "statusG";
+            this.statusG.Size = new System.Drawing.Size(27, 13);
+            this.statusG.TabIndex = 3;
+            this.statusG.Text = "OFF";
+            // 
+            // statusF
+            // 
+            this.statusF.AutoSize = true;
+            this.statusF.Location = new System.Drawing.Point(111, 75);
+            this.statusF.Name = "statusF";
+            this.statusF.Size = new System.Drawing.Size(27, 13);
+            this.statusF.TabIndex = 4;
+            this.statusF.Text = "OFF";
+            // 
+            // howToText
+            // 
+            this.howToText.AutoSize = true;
+            this.howToText.Location = new System.Drawing.Point(45, 9);
+            this.howToText.Name = "howToText";
+            this.howToText.Size = new System.Drawing.Size(176, 13);
+            this.howToText.TabIndex = 5;
+            this.howToText.Text = "Use keyboard keys A,S,D,F,G,H,I,K";
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.Location = new System.Drawing.Point(12, 51);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(14, 13);
+            this.labelC.TabIndex = 6;
+            this.labelC.Text = "C";
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = true;
+            this.labelD.Location = new System.Drawing.Point(45, 51);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(15, 13);
+            this.labelD.TabIndex = 7;
+            this.labelD.Text = "D";
+            // 
+            // labelE
+            // 
+            this.labelE.AutoSize = true;
+            this.labelE.Location = new System.Drawing.Point(78, 51);
+            this.labelE.Name = "labelE";
+            this.labelE.Size = new System.Drawing.Size(14, 13);
+            this.labelE.TabIndex = 8;
+            this.labelE.Text = "E";
+            // 
+            // labelF
+            // 
+            this.labelF.AutoSize = true;
+            this.labelF.Location = new System.Drawing.Point(111, 51);
+            this.labelF.Name = "labelF";
+            this.labelF.Size = new System.Drawing.Size(13, 13);
+            this.labelF.TabIndex = 9;
+            this.labelF.Text = "F";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Location = new System.Drawing.Point(144, 51);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(15, 13);
+            this.labelG.TabIndex = 10;
+            this.labelG.Text = "G";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(177, 51);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(14, 13);
+            this.labelA.TabIndex = 11;
+            this.labelA.Text = "A";
+            // 
+            // statusA
+            // 
+            this.statusA.AutoSize = true;
+            this.statusA.Location = new System.Drawing.Point(177, 75);
+            this.statusA.Name = "statusA";
+            this.statusA.Size = new System.Drawing.Size(27, 13);
+            this.statusA.TabIndex = 12;
+            this.statusA.Text = "OFF";
+            // 
+            // statusB
+            // 
+            this.statusB.AutoSize = true;
+            this.statusB.Location = new System.Drawing.Point(210, 75);
+            this.statusB.Name = "statusB";
+            this.statusB.Size = new System.Drawing.Size(27, 13);
+            this.statusB.TabIndex = 13;
+            this.statusB.Text = "OFF";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(210, 51);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(14, 13);
+            this.labelB.TabIndex = 14;
+            this.labelB.Text = "B";
+            // 
+            // statusC6
+            // 
+            this.statusC6.AutoSize = true;
+            this.statusC6.Location = new System.Drawing.Point(243, 75);
+            this.statusC6.Name = "statusC6";
+            this.statusC6.Size = new System.Drawing.Size(27, 13);
+            this.statusC6.TabIndex = 15;
+            this.statusC6.Text = "OFF";
+            // 
+            // labelC6
+            // 
+            this.labelC6.AutoSize = true;
+            this.labelC6.Location = new System.Drawing.Point(243, 51);
+            this.labelC6.Name = "labelC6";
+            this.labelC6.Size = new System.Drawing.Size(14, 13);
+            this.labelC6.TabIndex = 16;
+            this.labelC6.Text = "C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 111);
+            this.Controls.Add(this.labelC6);
+            this.Controls.Add(this.statusC6);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.statusB);
+            this.Controls.Add(this.statusA);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.labelF);
+            this.Controls.Add(this.labelE);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.labelC);
+            this.Controls.Add(this.howToText);
+            this.Controls.Add(this.statusF);
+            this.Controls.Add(this.statusG);
+            this.Controls.Add(this.statusE);
+            this.Controls.Add(this.statusD);
+            this.Controls.Add(this.statusC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusC;
+        private System.Windows.Forms.Label statusD;
+        private System.Windows.Forms.Label statusE;
+        private System.Windows.Forms.Label statusG;
+        private System.Windows.Forms.Label statusF;
+        private System.Windows.Forms.Label howToText;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.Label labelE;
+        private System.Windows.Forms.Label labelF;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label statusA;
+        private System.Windows.Forms.Label statusB;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label statusC6;
+        private System.Windows.Forms.Label labelC6;
+
+
+
+    }
+}
+
