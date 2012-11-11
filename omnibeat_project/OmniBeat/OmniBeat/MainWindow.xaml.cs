c65554b4c17b6966cb0afcba4adb963d7b799951
Daniel Barajas <i.am.barajas@gmail.com>
2012-11-10 18:15:24 -0800

    Created a new directory that contains the VS2010 Solution project for OmniBeat. - Erica: create UI elements with empty callbacks. - Devang/Theresa: Add callback functionality.

---
+++ omnibeat_project/OmniBeat/OmniBeat/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Multitouch.Framework.WPF.Input;
+using InteractiveSpace.SDK;
+using InteractiveSpace.SDK.DLL;
+
+namespace OmniBeat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Multitouch.Framework.WPF.Controls.Window
+    {
+        InteractiveSpaceProvider spaceProvider;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MultitouchScreen.AllowNonContactEvents = true;
+
+            spaceProvider = new InteractiveSpaceProviderDLL();
+            spaceProvider.Connect();
+
+            //Uncomment these lines to draw fingers on the projected screen
+            //spaceProvider.CreateFingerTracker();
+            //vizLayer.SpaceProvider = spaceProvider;
+        }
+
+        private void button_NewContact(object sender, NewContactEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Background = Brushes.OrangeRed;
+            b.Content = "Touching";
+        }
+
+        private void button_ContactRemoved(object sender, ContactEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Background = Brushes.White;
+            b.Content = "Touch Me!";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            spaceProvider.Close();
+        }
+
+    }
+}
