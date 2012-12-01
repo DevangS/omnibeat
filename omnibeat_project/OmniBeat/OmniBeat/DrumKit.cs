6dce584f3043d934c0793e5c34c42879087f588a
Helen Peng <penghelen@gmail.com>
2012-12-01 14:45:47 -0800

    adding new samples to drumkit

--- omnibeat_project/OmniBeat/OmniBeat/DrumKit.cs
+++ omnibeat_project/OmniBeat/OmniBeat/DrumKit.cs
@@ -11,20 +11,33 @@ namespace OmniBeat
     {
         private List<SampleSource> sampleSources;
         private WaveFormat waveFormat;
+        private string sampleDir = "Samples\\";
+        private string[] filenames = {"kick-trimmed", "snare-trimmed", "closed-hat-trimmed", "open-hat-trimmed",
+                                      "beats", "cymbal-and-drum", "cymbal", "dubwub", "electrohouse", "everybody",
+                                      "gangsta", "guitar", "help", "hip-hop-non-stop", "i-love-college", 
+                                      "never-gonna-give-you-up", "oh-yeah", "one-more-time", "scratch", "shots",
+                                      "swagga", "swangies", "youre-a-jerk" };
+
 
         public DrumKit()
         {
-            SampleSource kickSample = SampleSource.CreateFromWaveFile("Samples\\kick-trimmed.wav");
-            SampleSource snareSample = SampleSource.CreateFromWaveFile("Samples\\snare-trimmed.wav");
-            SampleSource closedHatsSample = SampleSource.CreateFromWaveFile("Samples\\closed-hat-trimmed.wav");
-            SampleSource openHatsSample = SampleSource.CreateFromWaveFile("Samples\\open-hat-trimmed.wav");
+            //SampleSource kickSample = SampleSource.CreateFromWaveFile("Samples\\kick-trimmed.wav");
+            //SampleSource snareSample = SampleSource.CreateFromWaveFile("Samples\\snare-trimmed.wav");
+            //SampleSource closedHatsSample = SampleSource.CreateFromWaveFile("Samples\\closed-hat-trimmed.wav");
+            //SampleSource openHatsSample = SampleSource.CreateFromWaveFile("Samples\\open-hat-trimmed.wav");
             sampleSources = new List<SampleSource>();
-
-            sampleSources.Add(kickSample);
-            sampleSources.Add(snareSample);
-            sampleSources.Add(closedHatsSample);
-            sampleSources.Add(openHatsSample);
-            this.waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(openHatsSample.SampleWaveFormat.SampleRate, openHatsSample.SampleWaveFormat.Channels);
+            SampleSource temp;
+            foreach (string s in filenames)
+            {
+                temp = SampleSource.CreateFromWaveFile(sampleDir + s + ".wav");
+                sampleSources.Add(temp);
+            }
+            temp = sampleSources.ElementAt(0);
+            //sampleSources.Add(kickSample);
+            //sampleSources.Add(snareSample);
+            //sampleSources.Add(closedHatsSample);
+            //sampleSources.Add(openHatsSample);
+            this.waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(temp.SampleWaveFormat.SampleRate, temp.SampleWaveFormat.Channels);
         }
 
         public virtual WaveFormat WaveFormat
