a93bd9289b6c3e1f74c75da8b76231abbb7668da
Theresa Calderon <tcaldero@ucsd.edu>
2012-12-01 17:00:19 -0800

    Pitch Controller working progress

---
+++ omnibeat_project/OmniBeat/OmniBeat/PitchController.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Multitouch.Framework.WPF.Input;
+using InteractiveSpace.SDK;
+using InteractiveSpace.SDK.DLL;
+namespace OmniBeat
+{
+    /// <summary>
+    /// Interaction logic for PitchController.xaml
+    /// </summary>
+    public partial class PitchController : UserControl
+    {
+        public PitchController()
+        {
+            InitializeComponent();
+        }
+    }
+}
