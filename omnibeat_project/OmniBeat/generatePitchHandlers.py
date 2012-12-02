91584eda3fb252d5bb206626b561ec507e6974ed
Theresa Calderon <tcaldero@ucsd.edu>
2012-12-01 23:23:20 -0800

    code Generators

--- omnibeat_project/OmniBeat/generatePitchHandlers.py
+++ omnibeat_project/OmniBeat/generatePitchHandlers.py
@@ -1,12 +1,12 @@
-template = """        private void rectangle%d%d_MouseDown(object sender, MouseButtonEventArgs e)
+template = """        private void pitch_NewContact%d%d(object sender, NewContactEventArgs e)
         {
-            ConfigClass.pitchGrid.grid[%d,%d].Click();
+            PitchController.pitchSequence.Click(%d, %d, state);
+            this.setPitch(MainWindow.chosenButton, %d, %d);
         }
 
 """
 maxCol = 8
 maxRow = 9
-with open("PitchHanders", "w") as pitchFile:
-  for col in range(maxCol):
-    for row in range(maxRow):
-      pitchFile.write(template % (col, row, col, row))
+for col in range(maxCol):
+  for row in range(maxRow):
+    print template % (col, row, col, row, col, row)
